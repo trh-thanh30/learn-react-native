db82c296dba4dc402edad0846b1f7c7194f49811
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-12-02 00:06:09 +0700

    flex-box

--- App.js
+++ App.js
@@ -1,22 +1,39 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const handleClick = () => {
+    Alert.alert("Hello");
+    // Add your code to save the todo to a database here
+  }
   return (
     <View style={styles.container}>
-      <Text style={{ color: "red" }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your to do" />
+       <Button color={"blue"} onPress={handleClick} title="Add todo" />
+      </View>
+      <View>
+        <Text>The todo list...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 10,
+    paddingTop: 100,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  textInput: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 10,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: "blue",
+    marginRight: 8,
+    width: "70%",
   },
 });
