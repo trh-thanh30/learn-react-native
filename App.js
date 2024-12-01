74df6f7eb8b926a27824ea2d2a7f8768ed9051bd
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-12-02 00:54:01 +0700

    render to do list

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
   const handleClick = () => {
-    Alert.alert("Hello");
-    // Add your code to save the todo to a database here
-  }
+    if (todo.trim()) {
+      setTodoList((currTodoList) => [...currTodoList, todo]);
+      setTodo("");
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your to do" />
-       <Button color={"blue"} onPress={handleClick} title="Add todo" />
+        <TextInput
+          value={todo}
+          onChangeText={setTodo}
+          style={styles.textInput}
+          placeholder="Your to do"
+        />
+        <Button color={"blue"} onPress={handleClick} title="Add todo" />
       </View>
-      <View>
-        <Text>The todo list...</Text>
-      </View>
+      {todoList.length <= 0 ? (
+        <Text style={styles.todoList}>Add to do list</Text>
+      ) : (
+        <View style={styles.todoList}>
+          {todoList.map((todo, index) => (
+            <Text style={styles.todoText} key={index}>
+              {index}: {todo}
+            </Text>
+          ))}
+        </View>
+      )}
     </View>
   );
 }
@@ -22,10 +40,17 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     paddingTop: 100,
+    flex: 1,
   },
   inputContainer: {
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    gap: 10,
+    marginBottom: 50,
+    borderBottomWidth: 1,
+    borderColor: "gray",
+    paddingBottom: 40,
+    flex: 1,
   },
   textInput: {
     flex: 1,
@@ -33,7 +58,22 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderWidth: 2,
     borderColor: "blue",
-    marginRight: 8,
     width: "70%",
+  },
+  todoList: {
+    flex: 4,
+  },
+  todoItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  todoText: {
+    padding: 10,
+    fontSize: 18,
+    marginBottom: 10,
+    backgroundColor: "lightgray",
+    color: "black",
+    borderRadius: 8,
   },
 });
