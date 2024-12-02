1904d0a41e0223b4cfa6fb49d96a2184f9b3e1c1
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-12-03 01:47:09 +0700

    bottom-tab-navigation

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Welcome to the Home Screen!</Text>
-      <Button
+      {/* <Button
         title="Open Details"
         onPress={() => {
           navigation.navigate("Details");
         }}
-      />
+      /> */}
     </View>
   );
 }
@@ -21,20 +23,46 @@ function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Welcome to the Details Screen!</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      {/* <Button title="Go to Home" onPress={() => navigation.navigate("Home")} /> */}
+    </View>
+  );
+}
+function SettingScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Setting Screen</Text>
+      {/* <Button title="Go back" onPress={() => navigation.goBack()} /> */}
     </View>
   );
 }
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size = 10 }) => {
+            let iconName;
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Details") {
+              iconName = "list";
+            } else {
+              iconName = "dashboard";
+            }
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+        <Stack.Screen name="Setting" component={SettingScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
